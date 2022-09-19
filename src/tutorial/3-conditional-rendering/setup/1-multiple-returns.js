86851ebd8c85ccabc07d92340559dd7692909238
iamkibeh <kibet.immanuel@student.moringaschool.com>
2022-09-19 20:20:42 +0300

    complete conditional rendering

--- src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -1,7 +1,49 @@
-import React, { useState, useEffect } from 'react';
-const url = 'https://api.github.com/users/QuincyLarson';
+import React, { useState, useEffect } from 'react'
+const url = 'https://api.github.com/users/QuincyLarson'
 const MultipleReturns = () => {
-  return <h2>multiple returns</h2>;
-};
+  const [isLoading, setIsLoading] = useState(true)
+  const [isError, setIsError] = useState(false)
+  const [user, setUser] = useState('def user')
 
-export default MultipleReturns;
+  useEffect(() => {
+    fetch(url)
+      .then((res) => {
+        if (res.status >= 200 && res.status < 300) {
+          return res.json()
+        } else {
+          setIsLoading(false)
+          setIsError(true)
+          throw new Error(res.statusText)
+        }
+      })
+      .then((user) => {
+        const { login } = user
+        setUser(login)
+        setIsLoading(false)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }, [])
+  if (isLoading) {
+    return (
+      <div>
+        <h1>loading ...</h1>
+      </div>
+    )
+  }
+  if (isError) {
+    return (
+      <div>
+        <h1>Error ...</h1>
+      </div>
+    )
+  }
+  return (
+    <div>
+      <h2>{user}</h2>
+    </div>
+  )
+}
+
+export default MultipleReturns
--- src/tutorial/3-conditional-rendering/setup/3-show-hide.js
+++ src/tutorial/3-conditional-rendering/setup/3-show-hide.js
@@ -1,7 +1,35 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'
 
 const ShowHide = () => {
-  return <h2>show/hide</h2>;
-};
+  const [show, setShow] = useState(false)
+  return (
+    <>
+      <button className='btn' onClick={() => setShow(!show)}>
+        show/hide
+      </button>
+      {show && <Item />}
+    </>
+  )
+}
 
-export default ShowHide;
+const Item = () => {
+  const [width, setWidth] = useState(window.innerWidth)
+
+  function checkSize() {
+    setWidth(window.innerWidth)
+  }
+  useEffect(() => {
+    window.addEventListener('resize', checkSize)
+    return () => {
+      window.removeEventListener('resize', checkSize)
+    }
+  }, [])
+  return (
+    <div>
+      <h1> window</h1>
+      <h2>size :{width}</h2>
+    </div>
+  )
+}
+
+export default ShowHide
