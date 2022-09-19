60dafec39b9e73edae55aa1f3a455b486c3e3415
iamkibeh <kibet.immanuel@student.moringaschool.com>
2022-09-19 20:22:04 +0300

    change App setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react'
-// import SetUp from './tutorial/1-useState/setup/5-useState-counter'
-import Final from './tutorial/1-useState/final/3-useState-array'
+import SetUp from './tutorial/4-forms/setup/2-multiple-inputs'
 function App() {
   return (
     <div className='container'>
-      {/* <SetUp /> */}
-      <Final />
+      <SetUp />
+      {/* <Final /> */}
     </div>
   )
 }
