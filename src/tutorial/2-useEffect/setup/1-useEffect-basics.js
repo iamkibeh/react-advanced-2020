58cb8940371e632e16fce7c71c53684d3cd6e261
iamkibeh <kibet.immanuel@student.moringaschool.com>
2022-09-19 20:20:04 +0300

    complete useEffect Hook

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -1,9 +1,25 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'
 // by default runs after every re-render
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
-};
+  const [count, setCount] = useState(0)
+  useEffect(() => {
+    console.log('call use effect')
+    if (count >= 1) {
+      document.title = `new message ${count}`
+    }
+  }, [count])
+  console.log('render component')
+  return (
+    <>
+      <h1>{count}</h1>
+      <h2>useEffect Basics</h2>
+      <div className='btn' onClick={() => setCount(count + 1)}>
+        click me
+      </div>
+    </>
+  )
+}
 
-export default UseEffectBasics;
+export default UseEffectBasics
--- src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
+++ src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
@@ -1,9 +1,42 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'
 
-const url = 'https://api.github.com/users';
+const url = 'https://api.github.com/users'
 
 const UseEffectFetchData = () => {
-  return <h2>fetch data</h2>;
-};
+  const [users, setUsers] = useState([])
 
-export default UseEffectFetchData;
+  // const gitUsers = async () => {
+  //   const resp = await fetch(url);
+  //   const users = await resp.json();
+  //   console.log(users)
+  // }
+  const gitUsers = () => {
+    fetch(url)
+      .then((resp) => resp.json())
+      .then((users) => setUsers(users))
+  }
+  useEffect(() => {
+    gitUsers()
+  }, [])
+  return (
+    <>
+      <h3>github users</h3>
+      <ul className='users'>
+        {users.map((user) => {
+          const { id, login, avatar_url, html_url } = user
+          return (
+            <li key={id}>
+              <img src={avatar_url} alt='' />
+              <div>
+                <h4>{login}</h4>
+                <a href={html_url}>profile</a>
+              </div>
+            </li>
+          )
+        })}
+      </ul>
+    </>
+  )
+}
+
+export default UseEffectFetchData
